ddfdfcb408e0f4a58a4e931a3b2c5a7313745b2d
SimplyEpicXI <aaronzconrad@gmail.com>
2023-11-06 10:53:11 -0500

    Module 4 solution

---
+++ module4-solution/inlineJS.js
@@ -0,0 +1,15 @@
+// document.write("This text is generate by JS" + "<br/>" + "The below text can also be seen in the console" + "<br/>" "<br/>");
+
+document.write("This is JavaScript text." + "<br/>" + "<br/>" + "The text below can also be seen in the console." + "<br/>" + "To validate the solution, please review files: " + "<br/>" + "i. script.js" + "<br/>" + "ii. SpeakHello.js" + "<br/>" + "iii. SpeakGoodBye.js" + "<br/>" + "<br/>");
+for (i = 0; i < names.length; i++) {	
+	var firstLetter = names[i].charAt(0);
+	firstLetter = firstLetter.toLowerCase();
+	if (firstLetter == "j") {
+ 		document.write("Good Bye " + names[i] + "<br/>");
+	} else {
+		document.write("Hello " + names[i] + "<br/>");
+	}
+}
+
+
+document.write("<br/>" + "<br/>" + "<br/>" + "Note: the solution used to generate the browser-visible JavaScript text varies slightly from the solution for the console text." + "<br/>" + "The console solution follows the instructions as per the assignment.")
